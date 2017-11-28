509aa5fd4dc41f32b5796117fe104f7c8b4206a9
Unknown <andrivgar@gmail.com>
2017-11-28 16:54:30 +0100

    Mejoras sudoku

--- Sudoku/SudokuFinalCheck.py
+++ Sudoku/SudokuFinalCheck.py
@@ -1,13 +1,14 @@
-import SudokuNumeroValidos
+import SudokuEsCuadrado
+import SudokuNumerosValidos
 import SudokuCheckFilas
 import SudokuCheckColumnas
 import TDDSudoku
-import SudokuEsCuadrado
+
 
 
 def finalCheck(sudoku):
     return SudokuEsCuadrado.esCuadrado(sudoku) and SudokuNumeroValidos.checkNumerosValidos(sudoku) \
-        and SudokuNumeroValidos.checkNumerosValidos(sudoku) and SudokuCheckFilas.checkFilas(sudoku) \
-        and SudokuCheckColumnas.checkColumnas(sudoku)
+        and SudokuCheckFilas.checkFilas(sudoku) and SudokuCheckColumnas.checkColumnas(sudoku)
 
-TDDSudoku.casosTest()
+
+TDDSudoku.functForTDD(True, finalCheck, "correcto")
